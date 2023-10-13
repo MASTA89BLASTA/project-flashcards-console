ef52eda3eac085c0027a7b02734c0b5ff2ba093c
HenryShuller <yuryshuller1996@gmail.com>
2023-10-13 21:42:03 +1000

    add ВЫ ОТВЕТИЛИ

--- modules/module.js
+++ modules/module.js
@@ -1,13 +1,18 @@
 //File Module
 
-const fs = require('fs').promises; //что бы была обработка через очередь промисифиц
-const { EOL } = require('os'); //для разных операционных систем
-const path = require('path'); //для правильного отображения слэшей
+const fs = require('fs').promises; // что бы была обработка через очередь промисифиц
+const { EOL } = require('os'); // для разных операционных систем символ конца строки 
+const path = require('path'); // для правильного отображения слэшей
 
 class Module {
   static async topicsNames() {
     const topics = await fs.readdir(path.join('../topics'));
-   console.log(topics.map((el) => el.replace('.txt', '').replaceAll("_", " "))) 
+    return topics.map((el) => el.replace('.txt', '').replaceAll('_', ' '));
+  }
+
+  static async getTopic(topicsName) {
+    const topic = await fs.readFile(`../topics/${topicsName}.txt`, 'utf-8');
+    return topic.split(EOL);
   }
 }
 Module.topicsNames();
