e21acc0315bb58e056f127efe7420e62d062b2f1
HenryShuller <yuryshuller1996@gmail.com>
2023-10-13 19:29:26 +1000

    add class MODULE

--- module/module.js
+++ module/module.js
@@ -1,1 +1,13 @@
 //File Module
+
+const fs = require('fs').promises; //что бы была обработка через очередь промисифиц
+const { EOL } = require('os'); //для разных операционных систем
+const path = require('path'); //для правильного отображения слэшей
+
+class Module {
+  static async topicsNames() {
+    const topics = await fs.readdir(path.join('../topics'));
+   console.log(topics.map((el) => el.replace('.txt', '').replaceAll("_", " "))) 
+  }
+}
+Module.topicsNames();
